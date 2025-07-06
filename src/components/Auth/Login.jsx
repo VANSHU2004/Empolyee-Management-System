de4b6b23c2e2bd8b376a1b8fbc062317caa7a74c
Atul Sharma <atulsharma02004@gmail.com>
2025-07-06 11:40:57 +0530

    Added handleLogin Function

--- src/components/Auth/Login.jsx
+++ src/components/Auth/Login.jsx
@@ -1,12 +1,13 @@
 import React, { useState } from 'react'
 
-const Login = () => {
+const Login = ({handleLogin}) => {
 
     const [email,setemail] = useState('')
     const [pass,setPass] = useState('')
 
     const submitHandler = (e)=>{
         e.preventDefault();
+        handleLogin(email, pass)
         setemail("")
         setPass("")
     }
